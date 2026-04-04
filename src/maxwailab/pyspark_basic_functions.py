eb84ce00f40817210a556d25ebd6fe594b90d11a
Max E L Wienandts <maxwienandts@gmail.com>
2026-04-04 07:30:12 -0300

    Fix import bug

--- src/maxwailab/pyspark_basic_functions.py
+++ src/maxwailab/pyspark_basic_functions.py
@@ -182,7 +182,7 @@ def pyspark_minmax_value(df: DataFrame, col: str):
         ) from e
 
 
-    df = df.filter(~(F.col(F.col).isNull() | isnan(F.col(F.col))))
+    df = df.filter(~(F.col(col).isNull() | isnan(F.col(col))))
     max_value = df.agg(F.max(F.col)).collect()[0][0]
     min_value = df.agg(F.min(F.col)).collect()[0][0]
 
